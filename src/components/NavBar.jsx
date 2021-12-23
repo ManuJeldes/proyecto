2b1672862a20b66436aa1daa102c483f66068ba6
ManuJeldes <m3jeldes@gmail.com>
2021-12-23 01:50:20 -0300

    Se comitea cracion de NanBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import logo from '../img/logo.png'
+
+const NavBar = () => {
+    return (
+        <div>
+           <nav className="logoNavConteiner">
+	
+    <div className="container">
+        <a href="index.html" className="logo">
+            <img src={logo} alt="Logo de cupcakes" width="80px" height="80px"/>
+        </a>
+    </div>
+
+
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li>
+            <a href="conocenos.html">Conócenos</a>
+        </li>
+        <li>
+            <a href="productos.html">Productos</a>
+        </li>
+        <li>
+            <a href="galeria.html">Galería </a>
+        </li>
+        <li>
+            <a href="contactanos.html">Contáctanos</a>
+        </li>
+        
+    </ul>
+</nav>
+        </div>
+    )
+}
+
+export default NavBar;
