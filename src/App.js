c7eb285ae7019a307b76d2b9c2c593fa92782cef
ManuJeldes <m3jeldes@gmail.com>
2021-12-30 23:49:06 -0300

    Se comitea counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
 import { Fragment } from 'react/cjs/react.production.min';
-
-
 
 function App() {
     return ( 
@@ -11,6 +10,7 @@ function App() {
         
             <NavBar/>
             <ItemListContainer/>
+            <ItemCount/>    
         
     </Fragment>
     );
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Fragment } from 'react/cjs/react.production.min'
 import carrito from '../img/carrito.png'
 
@@ -7,7 +8,7 @@ export function CartWidget (props) {
     return <Fragment>
         <div>
             <img src={carrito} alt="cartwidgets" width="40px" height="40px"/>
-            <span>{numOfItems}</span>
+            <span className=''>{numOfItems}</span>
         </div>
         </Fragment>
 }
